1a7382995eaf48f5f92f0bc76be70fde094105f9
BrendanCahill <brendancahill@gmail.com>
2017-04-07 15:12:22 -0700

    4/7/17 3:12 PM PST
    First time mobile build works

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+    name: 'WorkshopFinance',
+    description: 'The Art of Valuation',
+    author: 'Brendan F. Cahill',
+    email: 'brendan@workshopfinance.com',
+    website: 'http://workshopfinance.com'
+});
+
+App.setPreference('Orientation', 'all', 'ios');
