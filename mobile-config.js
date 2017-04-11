64971966698861cff9a0583afa69ca185408dd59
BrendanCahill <brendancahill@gmail.com>
2017-04-10 20:00:30 -0700

    4/10/17 8:00 PM PST
    Changed Valuation Block in mobile

--- mobile-config.js
+++ mobile-config.js
@@ -13,4 +13,17 @@ App.icons({
     // More screen sizes and platforms...
 });
 
+//App.launchScreens({
+//    'iphone_2x': 'public/workshop_finance_splash.png',
+//    'iphone5': 'public/workshop_finance_splash.png',
+//    'iphone6': 'public/workshop_finance_splash.png',
+//    'iphone6p_portrait': 'public/workshop_finance_splash.png',
+//    'iphone6p_landscape': 'public/workshop_finance_splash.png',
+//    'ipad_portrait': 'public/workshop_finance_splash.png',
+//    'ipad_portrait_2x': 'public/workshop_finance_splash.png',
+//    'ipad_landscape': 'public/workshop_finance_splash.png',
+//    'ipad_landscape_2x': 'public/workshop_finance_splash.png'
+//    // More screen sizes and platforms...
+//});
+//
 App.setPreference('Orientation', 'all', 'ios');
